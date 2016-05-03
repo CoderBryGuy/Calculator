f897e897ec994e011de56b918e0fb4f3377c6e93
CoderBryGuy <bryan.dov.bergman@gmail.com>
2016-05-03 12:43:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bryan/calculator/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.bryan.calculator;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button0 =(Button) findViewById(R.id.button0);
+        Button button1 =(Button) findViewById(R.id.button1);
+        Button button2 =(Button) findViewById(R.id.button2);
+        Button button3 =(Button) findViewById(R.id.button3);
+
+        Button button4 =(Button) findViewById(R.id.button4);
+        Button button5 =(Button) findViewById(R.id.button5);
+        Button button6 =(Button) findViewById(R.id.button6);
+
+        Button button7 =(Button) findViewById(R.id.button7);
+        Button button8 =(Button) findViewById(R.id.button8);
+        Button button9 =(Button) findViewById(R.id.button9);
+
+        Button buttonPlus     =(Button) findViewById(R.id.buttonPlus    );
+        Button buttonMinus    =(Button) findViewById(R.id.buttonMinus   );
+        Button buttonDivide   =(Button) findViewById(R.id.buttonDivide  );
+        Button buttonMultiply =(Button) findViewById(R.id.buttonMultiply);
+        Button buttonEquals   =(Button) findViewById(R.id.buttonEquals  );
+
+        Button buttonPeriod =(Button) findViewById(R.id.buttonPeriod);
+
+
+    button0.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("0");
+            }
+        });
+
+        button1.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("1");
+            }
+        });
+        button2.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("2");
+            }
+        });
+        button3.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("3");
+            }
+        });
+        button4.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("4");
+            }
+        });
+        button5.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("5");
+            }
+        });
+        button6.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("6");
+            }
+        });
+        button7.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("7");
+            }
+        });
+        button8.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("8");
+            }
+        });
+        button9.setOnClickListener( new Button.OnClickListener(){
+            public void onClick (View v)
+            {
+                TextView output = (TextView)findViewById(R.id.textView2);
+                output.append("9");
+            }
+        });
+    }
+}
